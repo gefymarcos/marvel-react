2d2548b67a3677f7426edba331e28ecb423b6c82
gefymarcos <gefymarcos@gmail.com>
2022-10-27 18:01:38 -0300

    feat(layout): ajustando layout mobile

--- src/App.tsx
+++ src/App.tsx
@@ -36,33 +36,35 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Saga do Infinito</h1>
+      <h1 className="AppTitle">Saga do Infinito</h1>
       
-      <div className="buttonBoxes">
+      <div>
         <div className="buttons">
           <p>Ordenar por:</p>
-          <button className={order === "RELEASE_DATE" ? "selected" : "default"} onClick={() => handleOrder("RELEASE_DATE")}>Ordem de lançamento</button>
-          <button className={order === "CHRONOLOGICAL" ? "selected" : "default"} onClick={() => handleOrder("CHRONOLOGICAL")}>Ordem cronológica</button>
+          <div>
+            <button className={order === "RELEASE_DATE" ? "selected" : "default"} onClick={() => handleOrder("RELEASE_DATE")}>Ordem de lançamento</button>
+            <button className={order === "CHRONOLOGICAL" ? "selected" : "default"} onClick={() => handleOrder("CHRONOLOGICAL")}>Ordem cronológica</button>
+          </div>
         </div>
-        <p>order by @rodrigo_souza_feliciano</p>
+        <p className="appOrderBy">order by @rodrigo_souza_feliciano</p>
       </div>
 
       <div className="cardBox">
-        {data.map((dt: any) => {
+        {data.map((dt: any, index: number) => {
           return (
-            <div className="card">
+            <div key={index} className="card">
               <h2  className="title">{dt.original_title}</h2>
               <div className="top">
-                <img src={`${IMAGE_URL}/${dt.poster_path}`} />
+                <img className="appImg" src={`${IMAGE_URL}/${dt.poster_path}`} />
                 <div className="iconBox">
                   <Icon path={`${IMDB_URL}${dt.imdb_id}`} alt="Ver no IMDB" icon="imdb" />
                   <Icon path={dt.homepage} alt="Ver na Marvel" icon="marvel" />
                 </div>
               </div>
-              <p key={dt.imdb_id}>
+              <div key={dt.imdb_id}>
                 <p className="overviewBox">{dt.overview}</p>
                 <p>Release date: {dt.release_date}</p>
-              </p>
+              </div>
             </div>
           )}
         )}
