f3d0f0b89d4209dde67b2037942105556b0a6c89
gefymarcos <gefymarcos@gmail.com>
2022-11-05 14:03:43 -0300

    refaq: create a type for movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import { useEffect, useState } from 'react'
 import { getMovies } from './api'
 import { IMAGE_URL, IMDB_URL } from './config'
 import { Icon } from './components/icon'
 import { sortByChronological, sortByReleaseDate } from './utils/sorter'
+import { Movie } from './data/types/movies'
 
 type OrderType = "CHRONOLOGICAL" | "RELEASE_DATE";
 
 function App() {
-  const [data, setData] = useState<any>([])
+  const [movies, setMovies] = useState<Movie[]>([]);
   const [order, setOrder] = useState<OrderType>("RELEASE_DATE");
 
   useEffect(() => {
     getMovies().then(result => {
-      setData(result ? result : [])
+      setMovies(result ? result : [])
       localStorage.setItem("data", JSON.stringify(result))
     });
   }, [])
@@ -26,9 +27,9 @@ function App() {
     setOrder(order);
 
     if (order === "RELEASE_DATE") {
-      setData(sortByReleaseDate(data));
+      setMovies(sortByReleaseDate(movies));
     } else {
-      setData(sortByChronological(data));
+      setMovies(sortByChronological(movies));
     }
 
     localStorage.setItem("order", order);
@@ -37,7 +38,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="AppTitle">Saga do Infinito</h1>
-      
+
       <div>
         <div className="buttons">
           <p>Ordenar por:</p>
@@ -50,20 +51,20 @@ function App() {
       </div>
 
       <div className="cardBox">
-        {data.map((dt: any, index: number) => {
+        {movies.map((movie: Movie, index: number) => {
           return (
             <div key={index} className="card">
-              <h2  className="title">{dt.original_title}</h2>
+              <h2  className="title">{movie.original_title}</h2>
               <div className="top">
-                <img className="appImg" src={`${IMAGE_URL}/${dt.poster_path}`} />
+                <img className="appImg" src={`${IMAGE_URL}/${movie.poster_path}`} />
                 <div className="iconBox">
-                  <Icon path={`${IMDB_URL}${dt.imdb_id}`} alt="Ver no IMDB" icon="imdb" />
-                  <Icon path={dt.homepage} alt="Ver na Marvel" icon="marvel" />
+                  <Icon path={`${IMDB_URL}${movie.imdb_id}`} alt="Ver no IMDB" icon="imdb" />
+                  <Icon path={movie.homepage} alt="Ver na Marvel" icon="marvel" />
                 </div>
               </div>
-              <div key={dt.imdb_id}>
-                <p className="overviewBox">{dt.overview}</p>
-                <p>Release date: {dt.release_date}</p>
+              <div key={movie.imdb_id}>
+                <p className="overviewBox">{movie.overview}</p>
+                <p>Release date: {movie.release_date}</p>
               </div>
             </div>
           )}
---
+++ src/data/types/movies.ts
@@ -0,0 +1,10 @@
+export type Movie = {
+  original_title: string;
+  poster_path: string;
+  release_date: string;
+  id: number;
+  imdb_id: string;
+  overview: string;
+  homepage: string;
+
+}
